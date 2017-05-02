cfe5e309edbd36ab1d5539c652eab29338d6be15
Andrew MacDonald <andrewmacdonald97@hotmail.com>
2017-05-01 23:22:10 -0300

    made the site a lot more minimal, updated projects in the projects section.

--- js/index.js
+++ js/index.js
@@ -1,25 +1,38 @@
-$(document).ready(function() {
-    $('#about-link').click(function() {
+$(document).ready(function () {
+    $('#about-link').click(function () {
         $('html, body').animate({
             scrollTop: $('#about').offset().top - $('#top-bar-wrapper').height()
         }, 500);
     });
-    $('#projects-link').click(function() {
+
+    $('#projects-link').click(function () {
         $('html, body').animate({
             scrollTop: $('#projects').offset().top - $('#top-bar-wrapper').height()
         }, 500);
     });
-    $('#contact-link').click(function() {
+
+    $('#contact-link').click(function () {
         $('html, body').animate({
             scrollTop: $('#contact').offset().top - $('#top-bar-wrapper').height()
         }, 500);
     });
+
+
+    $('#send-email-link').click(function () {
+        $('#email-address-val').html('andrewmacdonald97@hotmail.com');
+        $('#email-address-box').show();
+    });
+
+    $('#close-email-box').click(function () {
+        $('#email-address-box').hide();
+    });
 });
 
-$(document).scroll(function() {
-    if ($(this).scrollTop() > 10) {
-        $('#top-bar').addClass('shrinked');
-    } else {
-        $('#top-bar').removeClass('shrinked');
-    }
+$(document).scroll(function () {
+
+    // if ($(this).scrollTop() > 10) {
+    //     $('#top-bar').addClass('shrinked');
+    // } else {
+    //     $('#top-bar').removeClass('shrinked');
+    // }
 });
