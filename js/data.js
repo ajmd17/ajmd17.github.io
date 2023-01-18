8625b088f9f5cb023f08e0ad7935cbecbee82bf8
Andrew MacDonald <hello@andrewjmacdonald.ca>
2023-01-18 01:13:24 -0400

    update

--- js/data.js
+++ js/data.js
@@ -1,133 +1,125 @@
 var PORTFOLIO_DATA = {
   articles: [],
   projects: [{
-  title: 'Hyperion Engine',
-  description: '3D game engine written from scratch, using Vulkan and OpenGL as a backend',
+    title: 'Hyperion GameEngine',
+    description: 'Powerful 3D game engine written in C++17, rendering on Vulkan.',
 
-  img: ['screenshot71.png', 'screenshot65.png'],
-  links: [{
-      name: 'GitHub',
-      href: 'http://github.com/krait-games/hyperion-engine'
-  }]
+    img: ['hyp1.png'],
+    links: [{
+        name: 'GitHub',
+        href: 'http://github.com/krait-games/hyperion-engine'
+    }]
 
-},
-{
-    title: 'BidSquid Online Marketplace',
-    tags: ['Node.js', 'TypeScript', 'React', 'MongoDB'],
-    description: 'Online marketplace to connect buyers and sellers of agricultural goods. Includes a backend system for managing inventory and a procurement system. Read more: https://entrevestor.com/home/entry/hachey-traded-wall-street-for-agtech',
-    img: ['bidsquid_1.jpg', 'bidsquid_2.jpg', 'bidsquid_3.jpg'],
+  },{
+    title: 'TangOS',
+    description: 'A small operating system written in C for the fun of it.',
+
+    img: ['tangos.png'],
     links: [{
-      name: 'Visit Site',
-      main: true,
-      href: 'https://bidsquid.com'
+        name: 'GitHub',
+        href: 'https://github.com/ajmd17/TangOS'
     }]
-  }, {
-    title: 'Get Real!',
-    tags: ['Node.js', 'JavaScript'],
-    description: 'A website with a blog, audio player, events and merch for my band. All content is written in an extended form of markdown using a custom system.',
-    img: ['getreal_1.jpg'],
-    links: [{
-      name: 'Visit Site',
-      main: true,
-      href: 'https://getreal.band'
-    }]
-  }, {
-    title: 'Apex3D Game Engine',
-    tags: ['Game Engine', 'C++', 'OpenGL'],
-    description: 'OpenGL game engine written in C++.',
-    img: ['apex.jpg', 'world_terrain.PNG', 'reflections2.PNG', 'scout_trooper.PNG'],
-    links: [{
-      name: 'GitHub',
-      href: 'http://github.com/ajmd17/apex-engine'
-    }]
-  }, {
-    title: 'Lyric Video Editor',
-    tags: ['Node.js', 'JavaScript'],
-    description: 'A tool for building lyric videos, with a complex plugin system. I used this to make my own lyric videos.',
-    img: ['lyric_video_editor.jpg'],
-    links: [{
-      name: 'GitHub',
-      main: true,
-      href: 'https://github.com/ajmd17/lyric-video-editor'
-    }]
-  }, {
-    title: 'Celtic Colours app',
-    tags: ['React Native'],
-    description: 'Provides scheduling and event details for the Celtic Colours festival in Cape Breton. Contracted to work on this with Novastream.',
-    links: [{
-      name: 'GitHub',
-      main: true,
-      href: 'https://github.com/ajamiesonfraser/celticcoloursapp'
-    }]
-  }, {
-    title: 'Bandpageapp.com',
-    tags: ['Node.js', 'TypeScript', 'React', 'MongoDB'],
-    description: 'Crowdfunding platform for musicians',
-    img: ['bandpage_1.jpg', 'bandpage_2.jpg', 'bandpage_3.jpg']
-  }, {
-    title: 'BlockML - HTML Component Language',
-    tags: ['JavaScript'],
-    description: 'Makes writing HTML feel more consistent with the way we write JavaScript and CSS.',
-    img: ['blockml_1.jpg'],
-    links: [{
-      name: 'GitHub',
-      href: 'https://github.com/ajmd17/blockml'
+
+  },
+  {
+      title: 'BidSquid Online Marketplace',
+      tags: ['Node.js', 'TypeScript', 'React', 'MongoDB'],
+      description: 'Online marketplace to connect buyers and sellers of agricultural goods. Includes a backend system for managing inventory and a procurement system. Read more: https://entrevestor.com/home/entry/hachey-traded-wall-street-for-agtech',
+      img: ['bidsquid_1.jpg', 'bidsquid_2.jpg', 'bidsquid_3.jpg'],
+      links: [{
+        name: 'Visit Site',
+        main: true,
+        href: 'https://bidsquid.com'
+      }]
     }, {
-      name: 'GitHub (blockml-component)',
-      href: 'https://github.com/ajmd17/blockml-component'
-    }]
-  }, {
-    title: 'auto-clockify',
-    tags: ['Ruby'],
-    small: true,
-    description: 'A tool for automatically clocking worked hours based on git branches',
-    links: [{
-      name: 'GitHub',
-      href: 'https://github.com/ajmd17/auto-clockify'
-    }]
-  }, {
-    title: 'CoinTrends',
-    tags: ['Node.js', 'JavaScript'],
-    small: true,
-    description: 'Cryptocurrency price analysis toolkit with an extensive plugin system',
-    links: [{
-      name: 'GitHub',
-      href: 'https://github.com/ajmd17/cointrends'
-    }]
-  }, {
-    title: 'Vennio Project Manager',
-    tags: ['JavaScript'],
-    small: true,
-    description: 'A different take on project management - Focus on the visual components of a project.',
-    links: [{
-      name: 'Visit Site',
-      main: true,
-      href: 'https://ajmd17.github.io/vennio'
+      title: 'Get Real!',
+      tags: ['Node.js', 'JavaScript'],
+      description: 'A website with a blog, audio player, events and merch for my band. All content is written in an extended form of markdown using a custom system.',
+      img: ['getreal_1.jpg'],
+      links: [{
+        name: 'Visit Site',
+        main: true,
+        href: 'https://getreal.band'
+      }]
     }, {
-      name: 'GitHub',
-      href: 'http://github.com/ajmd17/vennio'
-    }]
-  }, {
-    title: 'Ace Programming Language',
-    tags: ['C++'],
-    small: true,
-    description: 'Progrmaming language written from scratch in C++. Includes bytecode language and virtual machine with garbage collection.',
-    links: [{
-      name: 'GitHub',
-      href: 'http://github.com/ajmd17/ace-lang'
-    }]
-  }, {
-    title: 'YTRoom',
-    tags: ['JavaScript', 'Firebase'],
-    small: true,
-    description: 'Watch videos with others in sync.',
-    links: [{
-      name: 'Visit Site',
-      main: true,
-      href: 'https://ajmd17.github.io/YTRoom'
+      title: 'Apex3D Game Engine',
+      tags: ['Game Engine', 'C++', 'OpenGL'],
+      description: 'OpenGL game engine written in C++.',
+      img: ['apex.jpg', 'world_terrain.PNG', 'reflections2.PNG', 'scout_trooper.PNG'],
+      links: [{
+        name: 'GitHub',
+        href: 'http://github.com/ajmd17/apex-engine'
+      }]
     }, {
-      name: 'GitHub',
-      href: 'http://github.com/ajmd17/ytroom'
+      title: 'Lyric Video Editor',
+      tags: ['Node.js', 'JavaScript'],
+      description: 'A tool for building lyric videos, with a complex plugin system. I used this to make my own lyric videos.',
+      img: ['lyric_video_editor.jpg'],
+      links: [{
+        name: 'GitHub',
+        main: true,
+        href: 'https://github.com/ajmd17/lyric-video-editor'
+      }]
+    }, {
+      title: 'Celtic Colours app',
+      tags: ['React Native'],
+      description: 'Provides scheduling and event details for the Celtic Colours festival in Cape Breton. Contracted to work on this with Novastream.',
+      links: [{
+        name: 'GitHub',
+        main: true,
+        href: 'https://github.com/ajamiesonfraser/celticcoloursapp'
+      }]
+    }, {
+      title: 'BlockML - Component Language',
+      tags: ['JavaScript'],
+      description: 'Makes writing HTML feel more consistent with the way we write JavaScript and CSS.',
+      img: ['blockml_1.jpg'],
+      links: [{
+        name: 'GitHub',
+        href: 'https://github.com/ajmd17/blockml'
+      }, {
+        name: 'GitHub (blockml-component)',
+        href: 'https://github.com/ajmd17/blockml-component'
+      }]
+    }, {
+      title: 'auto-clockify',
+      tags: ['Ruby'],
+      small: true,
+      description: 'A tool for automatically clocking worked hours based on git branches',
+      links: [{
+        name: 'GitHub',
+        href: 'https://github.com/ajmd17/auto-clockify'
+      }]
+    }, {
+      title: 'CoinTrends',
+      tags: ['Node.js', 'JavaScript'],
+      small: true,
+      description: 'Cryptocurrency price analysis toolkit with an extensive plugin system',
+      links: [{
+        name: 'GitHub',
+        href: 'https://github.com/ajmd17/cointrends'
+      }]
+    }, {
+      title: 'Vennio Project Manager',
+      tags: ['JavaScript'],
+      small: true,
+      description: 'A different take on project management - Focus on the visual components of a project.',
+      links: [{
+        name: 'Visit Site',
+        main: true,
+        href: 'https://ajmd17.github.io/vennio'
+      }, {
+        name: 'GitHub',
+        href: 'http://github.com/ajmd17/vennio'
+      }]
+    }, {
+      title: 'Ace Programming Language',
+      tags: ['C++'],
+      small: true,
+      description: 'Progrmaming language written from scratch in C++. Includes bytecode language and virtual machine with garbage collection.',
+      links: [{
+        name: 'GitHub',
+        href: 'http://github.com/ajmd17/ace-lang'
+      }]
     }]
-  }]
 }
